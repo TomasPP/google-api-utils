9461d721c4a1a941c2b2ea21de282ee9d2de7766
TomasPP <ltbrfree@gmail.com>
2020-11-08 14:47:41 +0200

    Initial commit

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 
 setup(
-    name="google_api_util",
+    name="google_api_utils",
     version="1.0.0",
     license="MIT",
     author="Tomas P",
     author_email="ttt@ttt.tt",
     url="...",
-    py_modules=["google_api_util"],
+    py_modules=["google_api_utils"],
     install_requires=[
                       "google-api-python-client>=1.8.2,<=2.0",
                       "google-auth-oauthlib>=0.4.1,<=1.0",
